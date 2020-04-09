3a7228320e2404d68c856d7feb8529d7a06fed7e
Andrew <andykamin3@gmail.com>
2020-04-09 19:17:16 -0300

    Added questions

--- js/sl.config.js
+++ js/sl.config.js
@@ -23,6 +23,287 @@ var config = [
         "Porque no habia harina"],
         correct: 3 
     },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Cuántos años estuvo el pueblo de israel en el desierto?",
+        answers: [
+        "120", 
+        "15", 
+        "40", 
+        "55"],
+        correct: 3 
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Los 4 nombres de Pesaj son:",
+        answers: [
+        "Jag HaAviv, Jag HaPesaj, Jag Hajerut, JagHamatzot", 
+        "Jag HaAviv, Jag HaPesaj, Jag Hajerut, Rosh Hashana", 
+        "Jag Sucot, Jag HaPesaj, Jag Hajerut, Purim", 
+        "Jag HaAviv, Jag HaPesaj, Jag HaShavuot, Jag HaSucot"],
+        correct: 1 
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Cómo se llamaba la hija de Paró?",
+        answers: [
+        "Myriam", 
+        "Tzipora", 
+        "Iojebed", 
+        "Batia"],
+        correct: 4 
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿De qué tribu era descendiente Moshé?",
+        answers: [
+        "Levi", 
+        "Cohen", 
+        "Israel", 
+        "Ninguna es correcta"],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Qué representan las mezuzot en nuestras puertas?",
+        answers: [
+        "Nuestra fe en D’s", 
+        "Recuerdan que D's salteo la casa de los hebreos durante las plagas en egipto", 
+        "Simplemente decoración", 
+        "Recuerdan las 10 plagas que D's arrojó sobre Egipto"],
+        correct: 2
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Cual es la principal diferencia entre pesaj y shavuot?",
+        answers: [
+        "En Pesaj se come matza y en Shavuot no", 
+        "En Shavuot festejamos la entrega de los diez mandamientos y en Pesaj la liberación de Egipto", 
+        "La Historia de Pesaj transcurre en Egipto y la de Shavuot en el desierto", 
+        "Todas son correctas"],
+        correct: 4 
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿A quién le toca cantar el ma nishtana?",
+        answers: [
+        "Al/la más alto/a de la familia", 
+        "Al/la más grande de la familia", 
+        "Al/la más chiquito/a de la familia", 
+        "Al/la que lo sepa mejor de la familia"],
+        correct: 3
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Sin Repetir y sin Soplar - Nombrá las 10 Plagas",
+        answers: [
+        "Pude", 
+        "No Pude", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Sin Repetir y sin Soplar - Elementos de la Keará",
+        answers: [
+        "Pude", 
+        "No Pude", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Sin Repetir y sin Soplar - Canciones de la Libertad ",
+        answers: [
+        "Pude", 
+        "No Pude", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Sin Repetir y sin Soplar - Los 4 Hijos de la Mesa de Pesaj son",
+        answers: [
+        "Pude", 
+        "No Pude", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Sin Repetir y sin Soplar - Momentos en los que se ven milagros en la Historia de Pesaj",
+        answers: [
+        "Pude", 
+        "No Pude", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "El hermano de Moshe se llamaba Aarón",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Moshe era tartamudo",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "La matza simboliza a las pirámides del antiguo Egipto.",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 2
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "El Jazeret es una pasta dulce a base de manzana y nuez.",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 2
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Moshé no aparece ni una vez en la Hagadá",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "El año segun la tora empieza en rosh hashana ",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 2
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Paró era el nombre del faraon",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 2
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "La esposa de Moshé se llamaba Tzipora",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "Pesaj se celebra durante 7 días en Israel y 8 días en la Diáspora",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "La hermana de Moshé se llamaba Batia",
+        answers: [
+        "Verdadero", 
+        "Falso", 
+        ],
+        correct: 2
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Cuantas matzot te comes en todo pesaj? ¿Con que te gusta comerlas?",
+        answers: [
+        "Pude Responder", 
+        "No Pude Responder", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Qué es lo que más te gusta de Pesaj?",
+        answers: [
+        "Pude Responder", 
+        "No Pude Responder", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Que te hace sentir libre?",
+        answers: [
+        "Pude Responder", 
+        "No Pude Responder", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Cuál es tu comida de Pesaj favorita?",
+        answers: [
+        "Pude Responder", 
+        "No Pude Responder", 
+        ],
+        correct: 1
+    },
+    { 
+        level: "1", 
+        theme: "Pesaj",
+        question: "¿Cuál es tu parte favorita de la historia de Pesaj?",
+        answers: [
+        "Pude Responder", 
+        "No Pude Responder", 
+        ],
+        correct: 1
+    },
+
+
+
+
     
     
     
